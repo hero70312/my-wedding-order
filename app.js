6e75c974c3ae24527949e56520eeb412212f3b43
Darren Chou <darren.chou@getoken.io>
2021-03-17 15:02:11 +0800

    all

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+
+// allow CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,locale");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
 app.use(express.static('public'));
 
 app.get('/index.html', function (req, res) {
