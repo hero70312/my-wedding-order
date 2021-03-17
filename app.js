d0dd3af33c14f6c6147dbcfc754e3d17f94e786f
Darren Chou <darren.chou@getoken.io>
2021-03-17 14:49:43 +0800

    index

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/index.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.html" );
+ })
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
